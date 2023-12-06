fd05a20f4fac79d20733c8183bb2b394924fe39c
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-12-06 19:16:40 +0200

    add chat id to users on creating chat

--- src/controllers/chats.ts
+++ src/controllers/chats.ts
@@ -3,18 +3,35 @@ import type { NextFunction, Request, Response } from "express";
 import chatsService from "../services/chats";
 import ChatDTO from "../dtos/chat";
 import { Message } from "../models/chat";
+import usersService from "../services/users";
+import FullUserDTO from "../dtos/fullUser";
 
 class ChatsController {
   async create(req: Request, res: Response, next: NextFunction) {
     try {
-      if (!Array.isArray(req.body.users)) {
+      const users = req.body.users;
+      if (!Array.isArray(users)) {
         return res.status(400).json({ message: "users property is not array" });
       }
-      if (req.body.users.length !== 2) {
+      if (users.length !== 2) {
         res.status(400).json({ message: "users length is not equal 2" });
       }
-      const createdChat = await chatsService.create(req.body.users);
+      const createdChat = await chatsService.create(users);
       const createdChatDTO = new ChatDTO(createdChat);
+
+      const firstUser = await usersService.getOne(users[0]);
+      const firstUserDTO = new FullUserDTO(firstUser);
+      await usersService.update({
+        ...firstUserDTO,
+        chats: [...firstUserDTO.chats, createdChat.id],
+      });
+
+      const secondUser = await usersService.getOne(users[1]);
+      const secondUserDTO = new FullUserDTO(secondUser);
+      await usersService.update({
+        ...secondUserDTO,
+        chats: [...secondUserDTO.chats, createdChat.id],
+      });
       res.status(200).json(createdChatDTO);
     } catch (err) {
       next(err);
